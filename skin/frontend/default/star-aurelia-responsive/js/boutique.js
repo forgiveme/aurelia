d6349d2e4181ea7e4454dae9752a6d048540f8b3
sunil verma <sanford@123789.org>
2016-09-16 18:37:10 +0530

    boutique.js change

--- skin/frontend/default/star-aurelia-responsive/js/boutique.js
+++ skin/frontend/default/star-aurelia-responsive/js/boutique.js
@@ -2,14 +2,14 @@ jQuery(window).load(function(){
 	jQuery(document).ready(function () {
 		jQuery('#btn_popup1').click(function() {
 			jQuery('html').addClass('overlay');
-			jQuery('.cms-aureliaboutiquedemo .header-container .desktop').css('z-index','0');
+			jQuery('.cms-aureliaboutique .header-container .desktop').css('z-index','0');
 			var activePopup = jQuery('#boutique-popup2-popup-sub');
 			jQuery(activePopup).addClass('visible');
 			window.scrollTo(0,0);
 		});
 		jQuery('#btn_popup2').click(function() {
 			jQuery('html').addClass('overlay');
-			jQuery('.cms-aureliaboutiquedemo .header-container .desktop').css('z-index','0');
+			jQuery('.cms-aureliaboutique .header-container .desktop').css('z-index','0');
 			var activePopup = jQuery('#boutique-popup1-popup-sub');
 			jQuery(activePopup).addClass('visible');
 			window.scrollTo(0,0);
